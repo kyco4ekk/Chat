d511bdb5ee9fa3f202b4d1364ab831911a3449c5
kyco4ekk <vanya.msk2364@gmail.com>
2024-03-19 14:53:38 +0300

    Add all files of project

---
+++ Client/DataBase/Config.cs
@@ -0,0 +1,9 @@
+﻿using Npgsql;
+
+namespace Client.DataBase
+{
+    internal class Config
+    {        
+        public string ConnectionString = "Host=localhost;Port=5433;Database=messenger;Username=postgres;Password=Vanyamsk23642";
+    }
+}            
